b3245eab5964fc7641f4da22312de149170182fa
nikolai <trumazafaka@gmail.com>
2025-08-30 17:10:47 +0300

    Added IdleHook

--- Core/Src/board_func.c
+++ Core/Src/board_func.c
@@ -4,6 +4,7 @@
  = File name:                    board_func.c
  ====================================================================
  */
+#include "stm32f4xx.h"
 #include "board_func.h"
 #include "cmsis_os.h"
 #include "FreeRTOS.h"
@@ -16,4 +17,15 @@
 void vRunHandlers(void)
 {
   //тут будет проверять что надо послать данные в CAN
+}
+
+/***************************************************************************/
+/**
+  * @brief  FreeRTOS Idle Hook: MCU sleep в простое
+  */
+void vApplicationIdleHook(void)
+{
+#if (configUSE_IDLE_HOOK == 1)
+    __WFI();
+#endif
 }
